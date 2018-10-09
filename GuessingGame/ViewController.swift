e6f0ef463cdc2e696b60d6dd4fbfa19cf00de799
Zachary Harper <zacharyharper@macbook-pro-8.local>
2018-10-09 09:52:24 -0500

    Initial Commit

---
+++ GuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessingGame
+//
+//  Created by Zachary Harper on 10/9/18.
+//  Copyright © 2018 Zachary Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
